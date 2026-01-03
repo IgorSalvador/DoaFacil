69a00f4af677e45d3ca5abdf4a98c221286b0065
Igor Henrique Salvador <igorsalvador0621@gmail.com>
2026-01-03 13:04:11 -0300

    Ajustes de convenções

--- src/DoaFacil.Infrastructure/Persistence/AppDbContext.cs
+++ src/DoaFacil.Infrastructure/Persistence/AppDbContext.cs
@@ -9,14 +9,14 @@ public class AppDbContext(DbContextOptions<AppDbContext> options)
     : IdentityDbContext<ApplicationUser, IdentityRole<long>, long, IdentityUserClaim<long>, IdentityUserRole<long>,
         IdentityUserLogin<long>, IdentityRoleClaim<long>, IdentityUserToken<long>>(options)
 {
-    public DbSet<DonationItem> DonationItems { get; set; }
-    public DbSet<DonationRequest> DonationRequests { get; set; }
-    public DbSet<DonationStatusHistory> DonationStatusHistory { get; set; }
+    public DbSet<DonationItem> DonationItems => Set<DonationItem>();
+    public DbSet<DonationRequest> DonationRequests => Set<DonationRequest>();
+    public DbSet<DonationStatusHistory> DonationStatusHistory => Set<DonationStatusHistory>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        base.OnModelCreating(builder);
-
         builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(builder);
     }
 }
